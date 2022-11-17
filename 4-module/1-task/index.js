0e259a3ab963ce2a27e701c0b2c103b738a52124
Pham Khanh <phamnhuquockhanh@yandex.ru>
2022-11-18 00:15:48 +0700

    add task#1-3 module#4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,8 @@
 function makeFriendsList(friends) {
   // ваш код...
+  let ul = document.createElement('UL');
+  for (let friend of friends) {
+    ul.innerHTML += `<li>${friend.firstName} ${friend.lastName}</li>`;
+  }
+  return ul
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,27 @@
 function highlight(table) {
   // ваш код...
+  for (let i = 1; i < table.rows.length; i++) {
+    let ageCells = table.rows[i].cells[1];
+    let genderCells = table.rows[i].cells[2];
+    let statusCells = table.rows[i].cells[3];
+    let age = ageCells.innerHTML;
+    let gender = genderCells.innerHTML;
+    let status = statusCells.dataset.available;
+    
+    if (age < 18) {
+      table.rows[i].style.textDecoration = 'line-through';
+    }
+    if (gender === 'm') {
+      table.rows[i].classList.add('male');
+    } else {
+      table.rows[i].classList.add('female');
+    }
+    if (status === 'true') {
+       table.rows[i].classList.add('available');
+    } else if (status === 'false') {
+       table.rows[i].classList.add('unavailable');
+    } else {
+       table.rows[i].hidden = true;
+    }
+  }
 }
