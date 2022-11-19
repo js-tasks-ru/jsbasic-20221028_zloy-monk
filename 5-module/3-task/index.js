97de44a13698fcdfdc75740f32d9acc9c2c1e03f
Pham Khanh <phamnhuquockhanh@yandex.ru>
2022-11-19 09:47:46 +0700

    описание того, что вы сделали

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,38 @@
 function initCarousel() {
   // ваш код...
+  let carouselArrow = document.querySelector('.carousel__arrow')
+  let arrowRight = document.querySelector('.carousel__arrow_right');
+  let arrowLeft = document.querySelector('.carousel__arrow_left');
+  let carousel = document.querySelector('.carousel__inner');
+  let slides = document.querySelectorAll('.carousel__slide').length;
+  let offsetWidth = carousel.offsetWidth;
+  let count = 0;
+
+  if (count === 0) {
+    arrowLeft.style.display = 'none';
+  }
+
+  arrowRight.addEventListener('click', () => {
+    if (count < slides - 1) {
+      count++;
+      carousel.style.transform = `translateX(-${offsetWidth * count}px)`;
+      if (count === slides - 1) {
+        arrowRight.style.display = 'none';
+      } else {
+        arrowLeft.style.display = '';
+      }
+    }
+  })
+
+  arrowLeft.addEventListener('click', () => {
+    if (count > 0) {
+      count--;
+      carousel.style.transform = `translateX(-${offsetWidth * count}px)`;
+      if (count === 0) {
+        arrowLeft.style.display = 'none';
+      } else {
+        arrowRight.style.display = '';
+      }
+    }
+  })
 }
